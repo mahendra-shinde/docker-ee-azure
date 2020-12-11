c2c700a554762ca7b4a569f3c12eb20fd1c6f145
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-12-11 17:22:20 +0530

    FixedAPT

--- scripts/docker-ee-install.sh
+++ scripts/docker-ee-install.sh
@@ -6,24 +6,22 @@ echo "10.0.0.4 master" | tee -a /etc/hosts
 echo "10.0.1.10 node-1" | tee -a /etc/hosts
 echo "10.0.1.11 node-2" | tee -a /etc/hosts
 
-curl -fsSL https://repos.mirantis.com/ubuntu/gpg |  apt-key add
+export DEBIAN_FRONTEND=noninteractive
+curl -fsSL https://repos.mirantis.com/ubuntu/gpg | apt-key add
 add-apt-repository "deb [arch=amd64] https://repos.mirantis.com/ubuntu  $(lsb_release -cs)  stable"
 apt update -y
 apt install docker-ee docker-ee-cli -y
 usermod -aG docker $USERNAME
 
 hostname=$(hostname)
-if [  $hostname == 'master' ]
+if [ $hostname == 'master' ]
 then
- echo "This is MASTER node, Installing the UCP"
- sudo docker image pull mirantis/ucp:3.3.2
- docker container run --rm -it --name ucp \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  mirantis/ucp:3.3.2 install \
-  --host-address 10.0.0.4 \
-  --san=$FQDN \
-  --admin-password=$ADMINPASS \
-  --admin-username=$USERNAME 
+
+echo "This is MASTER node, Installing the UCP"
+ 
+docker image pull mirantis/ucp:3.3.2
+
+docker container run --rm -it --name ucp  -v /var/run/docker.sock:/var/run/docker.sock mirantis/ucp:3.3.2 install --host-address 10.0.0.4 --san=$FQDN --admin-password=$ADMINPASS --admin-username=$USERNAME" 
 
 else
  echo "This is WORKER node"
