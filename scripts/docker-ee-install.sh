96dd37739c175bb5b6845119f7b3eb593651cab4
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-12-11 17:29:59 +0530

    ScriptOnLogin

--- scripts/docker-ee-install.sh
+++ scripts/docker-ee-install.sh
@@ -7,7 +7,7 @@ echo "10.0.1.10 node-1" | tee -a /etc/hosts
 echo "10.0.1.11 node-2" | tee -a /etc/hosts
 
 export DEBIAN_FRONTEND=noninteractive
-wget -q -O - https://repos.mirantis.com/ubuntu/gpg | apt-key add -
+wget  -qO - https://repos.mirantis.com/ubuntu/gpg | apt-key add -  
 add-apt-repository "deb [arch=amd64] https://repos.mirantis.com/ubuntu  $(lsb_release -cs)  stable"
 apt update -y
 apt install docker-ee docker-ee-cli -y
@@ -21,7 +21,7 @@ echo "This is MASTER node, Installing the UCP"
  
 docker image pull mirantis/ucp:3.3.2
 
-docker container run --rm -it --name ucp  -v /var/run/docker.sock:/var/run/docker.sock mirantis/ucp:3.3.2 install --host-address 10.0.0.4 --san=$FQDN --admin-password=$ADMINPASS --admin-username=$USERNAME" 
+echo "docker container run --rm -it --name ucp  -v /var/run/docker.sock:/var/run/docker.sock mirantis/ucp:3.3.2 install --host-address 10.0.0.4 --san=$FQDN --admin-password=$ADMINPASS --admin-username=$USERNAME" >> /home/$USERNAME/.bashrc
 
 else
  echo "This is WORKER node"
